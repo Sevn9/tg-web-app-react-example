b5af9dabac60c4b94494d38b21a36c58c8cc0790
Sevn9 <72407422+Sevn9@users.noreply.github.com>
2023-10-01 20:45:12 +0300

    added button and header styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import { useEffect } from "react";
 import "./App.css";
 
-const tg = window.Telegram.WebApp;
-
 function App() {
+  const tg = window.Telegram.WebApp;
   useEffect(() => {
     //собщает о том что произошла полная инициализации приложения
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
-  return (
-    <div className="App">
-      work
-      <button onClick={onClose}>Закрыть</button>
-    </div>
-  );
+  return <div className="App">work</div>;
 }
 
 export default App;
